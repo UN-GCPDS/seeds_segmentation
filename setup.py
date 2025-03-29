76a82bf96e9af3f41e686d220ab4f9ce3677110b
Immaczx <cpelaezg@unal.edu.co>
2025-03-29 07:04:17 -0500

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,14 @@
+# filepath: /setup.py
+from setuptools import setup, find_packages
+
+setup(
+    name="seeds_segmentation",
+    version="0.0.0",
+    description="Librería para segmentación de semillas",
+    author="UN-GCPDS-Camilo",
+    url="https://github.com/UN-GCPDS/seeds_segmentation",
+    packages=find_packages(),
+    install_requires=[
+        # Lista de dependencias necesarias
+    ],
+)
