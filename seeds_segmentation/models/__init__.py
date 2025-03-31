4c286bef5989669b80d2c61b6a5dde82a25c916b
Immaczx <cpelaezg@unal.edu.co>
2025-03-30 21:48:40 -0500

    fix models __init__.py

--- seeds_segmentation/models/__init__.py
+++ seeds_segmentation/models/__init__.py
@@ -8,6 +8,6 @@ from .baseline_unet import unet_baseline
 from .baseline_segnet import segnet_baseline
 from .baseline_res_unet import res_unet_baseline
 from .mobilenetv2 import mobilenetv2
-from .mobilenetv2_resunet import res_unet_mobilenetv2
+from .mobilenetv2_res_unet import res_unet_mobilenetv2
 from .mobilenetv2_unet import unet_mobilenetv2
 from .vgg16_unet import unet_vgg16
--- seeds_segmentation/models/mobilenetv2_unet.py
+++ seeds_segmentation/models/mobilenetv2_unet.py
@@ -49,7 +49,7 @@ def get_decoder(skips,dropout=0):
         x = tf.keras.layers.Concatenate()([x,skip])
     return x
 #----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
-def unet_mobilenetvc2(input_shape=(128,128,3), out_channels=1, out_ActFunction='sigmoid', trainable = False, name="unetMobile"):
+def unet_mobilenetv2(input_shape=(128,128,3), out_channels=1, out_ActFunction='sigmoid', trainable = False, name="unetMobile"):
     input = tf.keras.layers.Input(shape=input_shape)
 
     skips = get_encoder(input_shape=list(input.shape[1:]),  trainable = trainable)(input)
