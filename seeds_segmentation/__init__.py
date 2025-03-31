dc14f329e13e3ad09b910329ae304d5bdd06724b
Immaczx <cpelaezg@unal.edu.co>
2025-03-30 22:04:01 -0500

    list models outside the models submodule

--- seeds_segmentation/__init__.py
+++ seeds_segmentation/__init__.py
@@ -4,4 +4,4 @@ Seed Segmentation
 ==========================
 """
 # Import models from the submodule
-from .models import unet_baseline, fcn_baseline, segnet_baseline, res_unet_baseline
+from .models import unet_baseline, fcn_baseline, res_unet_baseline, unet_mobilenetv2, res_unet_mobilenetv2, mobilenetv2
